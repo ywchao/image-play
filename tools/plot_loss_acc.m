eb92a653aff928fb2bec41d668d695acaf8dff0c
chao <chao@adobe.com>
2016-07-27 17:24:31 -0700

    plot loss acc + vis pred video

---
+++ tools/plot_loss_acc.m
@@ -0,0 +1,144 @@
+figure(1);
+
+% choose experiment
+
+% exp_name = 'seq16-hg-single-no-skip-clstm';
+% exp_name = 'seq16-hg-single-no-skip-res-clstm';
+
+% set parameters
+disp_int = 1000;
+max_epoch = 4;
+
+epoch_size = 19690;
+seq_length = 16;
+format = ['%s %s %s %s' repmat(' %s %s',[1,seq_length])];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+log_file = ['./exp/penn-crop/' exp_name '/train.log'];
+f = fopen(log_file);
+C = textscan(f,format);
+fclose(f);
+epoch = cellfun(@(x)str2double(x),C{1}(2:end));
+iter = cellfun(@(x)str2double(x),C{2}(2:end));
+loss = zeros(numel(iter),seq_length);
+acc = zeros(numel(iter),seq_length);
+for i = 1:seq_length
+    loss(:,i) = cellfun(@(x)str2double(x),C{i+4}(2:end));
+    acc(:,i) = cellfun(@(x)str2double(x),C{i+seq_length+4}(2:end));
+end
+
+ind = [];
+for i = 1:max_epoch
+    ii = find(epoch == i);
+    if isempty(ii)
+        continue
+    end
+    % sample index uniformly
+    ind = [ind; ii(1:disp_int:numel(ii))];  %#ok
+    % add the last iter of each epoch
+    if ismember(epoch_size, ii) && ~ismember(epoch_size, ind)
+        ind = [ind; epoch_size];  %#ok
+    end
+end
+it = (epoch(ind)-1)*epoch_size + iter(ind);
+
+subplot(2,2,1);
+plot(it,loss(ind,1),'--r'); hold on;
+plot(it,loss(ind,2),'r');
+for i = 3:16
+    it_t = it(loss(ind,i) ~= 0);
+    ind_t = ind(loss(ind,i) ~= 0);
+    clr = (i-2)/(seq_length-2);
+    % clr = (i-2)/(8-2);
+    plot(it_t,loss(ind_t,i),'color',[1-clr,0,clr]);
+end
+grid on;
+title('training loss');
+xlabel('iteration');
+
+subplot(2,2,2);
+plot(it,acc(ind,1),'--r'); hold on;
+plot(it,acc(ind,2),'r');
+for i = 3:16
+    it_t = it(~isnan(acc(ind,i)));
+    ind_t = ind(~isnan(acc(ind,i)));
+    clr = (i-2)/(seq_length-2);
+    % clr = (i-2)/(8-2);
+    plot(it_t,acc(ind_t,i),'color',[1-clr,0,clr]);
+end
+grid on;
+title('training accuracy');
+xlabel('iteration');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+log_file = ['./exp/penn-crop/' exp_name '/val.log'];
+f = fopen(log_file);
+C = textscan(f,format);
+fclose(f);
+epoch = cellfun(@(x)str2double(x),C{1}(2:end));
+iter = cellfun(@(x)str2double(x),C{2}(2:end));
+loss = zeros(numel(iter),seq_length);
+acc = zeros(numel(iter),seq_length);
+for i = 1:seq_length
+    loss(:,i) = cellfun(@(x)str2double(x),C{i+4}(2:end));
+    acc(:,i) = cellfun(@(x)str2double(x),C{i+seq_length+4}(2:end));
+end
+
+it = (epoch-1)*epoch_size + iter;
+
+subplot(2,2,1);
+lim = [xlim, ylim];
+subplot(2,2,3);
+plot(it,loss(:,1),'--ro'); hold on;
+plot(it,loss(:,2),'-ro');
+for i = 3:16
+    it_t = it(loss(:,i) ~= 0);
+    ind_t = loss(:,i) ~= 0;
+    clr = (i-2)/(seq_length-2);
+    % clr = (i-2)/(8-2);
+    plot(it_t,loss(ind_t,i),'-o','color',[1-clr,0,clr]);
+end
+axis(lim);
+grid on;
+title('validation loss');
+xlabel('iteration');
+
+subplot(2,2,2);
+lim = [xlim, ylim];
+subplot(2,2,4);
+plot(it,acc(:,1),'--ro'); hold on;
+plot(it,acc(:,2),'-ro');
+for i = 3:16
+    it_t = it(~isnan(acc(:,i)));
+    ind_t = ~isnan(acc(:,i));
+    clr = (i-2)/(seq_length-2);
+    % clr = (i-2)/(8-2);
+    plot(it_t,acc(ind_t,i),'-o','color',[1-clr,0,clr]);
+end
+axis(lim);
+grid on;
+title('validation accuracy');
+xlabel('iteration');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+subplot(2,2,1); set(gca,'fontsize',8);
+subplot(2,2,2); set(gca,'fontsize',8);
+subplot(2,2,3); set(gca,'fontsize',8);
+subplot(2,2,4); set(gca,'fontsize',8);
+
+subplot(2,2,1);
+lim = [xlim, ylim];
+subplot(2,2,3);
+axis(lim);
+subplot(2,2,2);
+lim = [xlim, ylim];
+subplot(2,2,4);
+axis(lim);
+
+% save to file
+save_file = ['outputs/plot_' exp_name '.pdf'];
+if ~exist(save_file,'file')
+    set(gcf,'PaperPosition',[0 0 8 6]);
+    print(gcf,save_file,'-dpdf');
+end
+
+close;
