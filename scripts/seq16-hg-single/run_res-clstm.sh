8f878f1b7b8a48c8165e2db2f7cd83e3f04ad283
chao <chao@adobe.com>
2016-07-29 18:22:29 -0700

    hg single

---
+++ scripts/seq16-hg-single/run_res-clstm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  gpu_id=0
+else
+  gpu_id=$1
+fi
+
+th main.lua \
+  -GPU $gpu_id \
+  -expID seq16-hg-single-res-clstm \
+  -nEpochs 4 \
+  -batchSize 1 \
+  -testInt 20000 \
+  -netType hg-single-res-clstm \
+  -hgModel pose-hg-train/exp/penn_action_cropped/hg-single-ft/best_model.t7
