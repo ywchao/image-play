0f6ae4caed07b35fb54e9762db04f098864b5681
Yu-Wei Chao <ywchao@umich.edu>
2023-07-31 21:50:25 -0700

    Update links

--- scripts/fetch_implay_models_prediction.sh
+++ scripts/fetch_implay_models_prediction.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )/exp"
 mkdir -p $DIR && cd $DIR
 
 FILE=precomputed_implay_models_prediction.tar.gz
-ID=1VnDJRWuzff6hgcvhDDqYdSJd0NNru3cY
+ID=1_kvscL7HKua9r_XU67iCSGqG8WbqT7-k
 
 if [ -f $FILE ]; then
   echo "File already exists..."
